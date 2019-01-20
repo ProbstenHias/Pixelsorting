7e5e7dfa8115e57cc7b4554d590e784ae11fb6b8
ProbstenHias <matthias.weilinger@gmx.de>
2019-01-20 20:32:58 +0100

    initial commit

---
+++ src/MySketch.java
@@ -0,0 +1,53 @@
+import processing.core.PApplet;
+import processing.core.PImage;
+
+public class MySketch extends PApplet {
+
+    PImage img;
+    PImage sorted;
+    int index = 0;
+
+    public static void main(String[] args) {
+        String[] processingArgs = {"MySketch"};
+        MySketch mySketch = new MySketch();
+        PApplet.runSketch(processingArgs, mySketch);
+    }
+
+    public void settings() {
+        img = loadImage("images/image1.jpg");
+        img.resize(img.width / 2, img.height / 2);
+        size(img.width, img.height);
+        sorted = createImage(img.width, img.height, RGB);
+        sorted = img.get();
+
+    }
+
+    public void setup() {
+        frameRate(60);
+    }
+
+    public void draw() {
+        println(frameRate);
+        for (int i = 0; i < 5; i++) {
+            image(sorted, 0, 0);
+            int selected = index;
+            float biggest = -1;
+            for (int j = index; j < sorted.pixels.length; j++) {
+                if (hue(sorted.pixels[j]) > biggest) {
+                    selected = j;
+                    biggest = hue(sorted.pixels[j]);
+                }
+            }
+
+            int tmp = sorted.pixels[selected];
+            sorted.pixels[selected] = sorted.pixels[index];
+            sorted.pixels[index] = tmp;
+            sorted.updatePixels();
+
+            if (index < sorted.pixels.length) {
+                index++;
+            }
+        }
+
+    }
+}
